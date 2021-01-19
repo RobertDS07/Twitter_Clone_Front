474a5219d5b21008a29544df386190800fd9ed79
Robert Damaceno <robertdamacenobob@gmail.com>
2021-01-18 21:02:59 -0300

    Add feed

--- src/components/Feed/index.jsx
+++ src/components/Feed/index.jsx
@@ -1,5 +1,35 @@
 import React from 'react'
 
+import { Grid, Paper, makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles({
+    wrapperTextArea: {
+        height: '200px',
+        width: '100%',
+        margin: '20px 0',
+    },
+    paperTextArea: {
+        height: '200px',
+    },
+})
+
 export default () => {
-    return <h1>Feed</h1>
+    const { paperTextArea, wrapperTextArea } = useStyles()
+
+    return (
+        <Grid item xs={12}>
+            <Grid
+                container
+                justify="flex-start"
+                direction="column"
+                alignItems="center"
+            >
+                <Grid item xs={10} sm={7} className={wrapperTextArea}>
+                    <Paper className={paperTextArea} elevation={3}>
+                        {/* form  */}
+                    </Paper>
+                </Grid>
+            </Grid>
+        </Grid>
+    )
 }
